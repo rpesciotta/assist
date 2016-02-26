2934912eb8b0ea78441578ad843c7fe84a5c691a
Ricardo Pesciotta <ricardo.pesciotta@gmail.com>
2016-02-26 19:33:16 +0000

    Initial code for inserting a new ticket

--- client/helpers.js
+++ client/helpers.js
@@ -12,6 +12,13 @@ Template.helpdesk.helpers({
    }
 });
 
+Template.helpdesk.events({
+   'click .js-newticket': function(event) {
+      event.preventDefault();
+      Template.render('newTicket');
+   }
+});
+
 Template.ticketItem.helpers({
    'firstMsg': function(id){
       return String(TicketHistory.findOne({ticketId:id}).data).substring(0,50);
