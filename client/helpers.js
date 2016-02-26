0c6b9d58d4dc1d7fa8ed9065a69ef387ba2587c6
Ricardo Pesciotta <ricardo.pesciotta@gmail.com>
2016-02-26 17:49:35 +0000

    added collectionfs and gridfs, customized accounts-ui to include username, updated shared helpers code, trying to read isAdmin prop from user - not working

--- client/helpers.js
+++ client/helpers.js
@@ -1,10 +1,7 @@
 Template.registerHelper("isHelpdeskUser", function(){
-   if (Meteor.user()) {
-      //var currentUser = users.findOne(Meteor.userId());
-      var currentUser = Meteor.user();
-      console.log(currentUser);
-      return !!currentUser.isAdmin;
-   }
-   else
-      return false;
-})
+   return Meteor.user() && !!Meteor.user().isAdmin;
+});
+
+Template.registerHelper('formatDate', function(date) {
+	return moment(date).format('lll');
+});
--- server/startup.js
+++ server/startup.js
@@ -1,18 +1,20 @@
 Meteor.startup(function () {
   //create admin user
-  if ( Meteor.users.find({username: 'admin'}).count() === 0 ) {
-    let admin = Accounts.createUser({
+  if ( !Meteor.users.findOne({username: 'admin'}) ) {
+     console.log('Creating admin user in the database');
+    let adminId = Accounts.createUser({
         username: 'admin',
         email: 'admin@meteorinflipflops.com',
         password: 'admin',
         profile: {
             first_name: 'Admin',
             last_name: 'Assist',
-            company: 'Assist',
+            company: 'Assist'
         }
     });
     // store admin role on user
-    Meteor.users.update({_id:admin}, {$set: {isAdmin: true}});
+    Meteor.users.update({_id:adminId}, {$set: {isAdmin: true}});
+    //console.log(Meteor.users.findOne({username:'admin'}));
   }
 
 });
