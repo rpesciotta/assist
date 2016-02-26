11a8695b1fbdaf99f2e14722ef59c33aba048791
Ricardo Pesciotta <ricardo.pesciotta@gmail.com>
2016-02-26 16:55:33 +0000

    Small changes in templates and helpers to make initial works

--- client/helpers.js
+++ client/helpers.js
@@ -1,3 +1,10 @@
 Template.registerHelper("isHelpdeskUser", function(){
-  return users.findOne({_id:Meteor.userId()}).count();
+   if (Meteor.user()) {
+      //var currentUser = users.findOne(Meteor.userId());
+      var currentUser = Meteor.user();
+      console.log(currentUser);
+      return !!currentUser.isAdmin;
+   }
+   else
+      return false;
 })
