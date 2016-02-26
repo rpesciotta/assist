e83c8b3323f0a8dfbd2f229abd3daa8cb042ba2d
Ricardo Pesciotta <ricardo.pesciotta@gmail.com>
2016-02-26 17:04:51 +0000

    Fixed admin user creation code - updated main temporary page

--- server/startup.js
+++ server/startup.js
@@ -1,6 +1,6 @@
 Meteor.startup(function () {
   //create admin user
-  if ( Meteor.users.find({name: 'admin'}).count() === 0 ) {
+  if ( Meteor.users.find({username: 'admin'}).count() === 0 ) {
     let admin = Accounts.createUser({
         username: 'admin',
         email: 'admin@meteorinflipflops.com',
